e05e5e21c0a14eb0b1ac1a64f140af37505649c1
Daniel <Brenda@ELIZABETH>
2016-09-14 12:57:16 -0500

    Creada la solución API, además se agregadó la cadena de conexión y el Contexto.

---
+++ INEQ_API/INEQ_API/Models/INEQAPIContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace INEQ_API.Models
+{
+    public class INEQAPIContext : DbContext
+    {
+        public INEQAPIContext() : base("INEQAPIContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
